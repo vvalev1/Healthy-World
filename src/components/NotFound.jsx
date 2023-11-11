e3fabdb39a70c4eea5d3de3a722720ef846e6e13
Valentin Valev <valio.94.v@gmail.com>
2023-11-11 20:27:52 +0200

    add link to home page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+import Home from "./Home"
 export default function ErrorComponent() {
     return(
         <div className="container-xxl py-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -8,7 +10,7 @@ export default function ErrorComponent() {
                         <h1 className="display-1">404</h1>
                         <h1 className="mb-4">Page Not Found</h1>
                         <p className="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
-                        <a className="btn btn-primary rounded-pill py-3 px-5" href="">Go Back To Home</a>
+                        <Link to='/' element={<Home/>} className="btn btn-primary rounded-pill py-3 px-5" >Go Back To Home</Link>
                     </div>
                 </div>
             </div>
