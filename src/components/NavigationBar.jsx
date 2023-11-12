298729d2bc19dba78f057151a0ebfd2032797695
Valentin Valev <valio.94.v@gmail.com>
2023-11-12 18:48:45 +0200

    modify scroll function to fix top nav bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -6,19 +6,19 @@ export default function NavigationBarComponent() {
     return(
         
         <div className="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
-            {/* <div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
-                <div class="col-lg-6 px-5 text-start">
+             {/* <div class="top-bar row gx-0 align-items-center d-none d-lg-flex"> */}
+                {/* <div class="col-lg-6 px-5 text-start">
                     <small><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York, USA</small>
                     <small class="ms-4"><i class="fa fa-envelope me-2"></i>info@example.com</small>
-                </div>
-                <div class="col-lg-6 px-5 text-end">
+                </div> */}
+                {/* <div class="col-lg-6 px-5 text-end">
                     <small>Follow us:</small>
                     <a class="text-body ms-3" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="text-body ms-3" href=""><i class="fab fa-twitter"></i></a>
                     <a class="text-body ms-3" href=""><i class="fab fa-linkedin-in"></i></a>
                     <a class="text-body ms-3" href=""><i class="fab fa-instagram"></i></a>
-                </div>
-            </div> */}
+                </div> */}
+            {/* </div> */}
     
             <nav className="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
                 <Link to="/" element={<Home/>} className="navbar-brand ms-4 ms-lg-0">
