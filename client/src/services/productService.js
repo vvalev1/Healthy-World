ed17d6e43a355b2b667d0eab3eb16426930c927c
Valentin Valev <valio.94.v@gmail.com>
2023-11-18 23:52:36 +0200

    add getAll fetch in productService

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -13,4 +13,12 @@ export const create = async (productData) => {
     const result = await response.json();
     return result;
 
-}
+};
+
+export const getAll = async () => {
+    const response = await fetch(`${baseUrl}/products`);
+
+    const result = await response.json();
+    console.log(result)
+    return result;
+};
