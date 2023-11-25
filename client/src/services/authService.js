cf80c174968700f6bca2e0e17ae7b82c53ddca50
Valentin Valev <valio.94.v@gmail.com>
2023-11-25 18:03:44 +0200

    add auth service

---
+++ client/src/services/authService.js
@@ -0,0 +1,20 @@
+const baseUrl = "http://localhost:3030/users";
+
+export const login = async (email, password) => {
+
+    const response = await fetch(`${baseUrl}/login`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+
+    const result = await response.json();
+
+
+    return result;
+}
