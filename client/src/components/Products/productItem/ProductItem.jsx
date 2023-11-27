79c9d890a28d0e893f4c3365058d55ddb22adba6
Valentin Valev <valio.94.v@gmail.com>
2023-11-28 01:01:52 +0200

    link img to product details

--- client/src/components/Products/productItem/ProductItem.jsx
+++ client/src/components/Products/productItem/ProductItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Path from "../../../paths/paths";
 
 export default function ProductItem(
     { name, price, imageUrl }
@@ -7,10 +8,10 @@ export default function ProductItem(
                 <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div className="product-item">
                         <div className="position-relative bg-light overflow-hidden">
-                            <img className="img-fluid w-100" style={{height: "20em"}} src={imageUrl} alt="" />
+                        <Link to={Path.ProductDetails}><img className="img-fluid w-100" style={{height: "20em"}} src={imageUrl} alt="" /></Link>
                         </div>
                         <div className="text-center p-4">
-                            <Link to="/products/details" className="d-block h5 mb-2">{name}</Link>
+                            <Link to={Path.ProductDetails} className="d-block h5 mb-2">{name}</Link>
                             <span className="text-primary me-1">${price}</span>
                         </div>
                     </div>
