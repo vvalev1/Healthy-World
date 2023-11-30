d0c54f1971d50c628cb1549713f767cc466c387e
Valentin Valev <valio.94.v@gmail.com>
2023-12-01 00:03:35 +0200

    add authentication guards

--- client/src/components/Products/details/ProductDetails.jsx
+++ client/src/components/Products/details/ProductDetails.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import * as productService from "../../../services/productService"
 import Path from "../../../paths/paths";
+import AuthContext from "../../contexts/AuthContext";
 
 import Header from "../../Header";
 import styles from "./ProductDetails.module.css"
@@ -11,6 +12,7 @@ export default function Details() {
     const navigate = useNavigate();
     const [product, setProduct] = useState({});
     const { productId } = useParams();
+    const { isAuthenticated } = useContext(AuthContext);
 
 
     useEffect(() => {
@@ -29,6 +31,14 @@ export default function Details() {
         }
     }
 
+    const buyProductHandler = () => {
+        if(!isAuthenticated) {
+            navigate(Path.Login);
+        }
+
+        // TODO: buy a product 
+    }
+
     return (
         <>
             <Header
@@ -44,10 +54,12 @@ export default function Details() {
                             <div className="col-lg-7 col-md-9 col-sm-4">
                                 <h2 className=" mt-5">{product.name}</h2>
                                 <h4 className="mt-5">Price: <span>${product.price}</span></h4>
-                                <button className="btn btn-primary">Buy Now</button>
+                                <button className="btn btn-primary" onClick={buyProductHandler}>Buy Now</button>
                                 <div className="mt-4">
-                                    <Link to={Path.EditProduct} className="btn btn-secondary mx-2 px-4 rounded-pill">Edit</Link>
-                                    <button className="btn btn-danger rounded-pill px-4" onClick={deleteProductHandler}>Delete</button>
+                                    {isAuthenticated && (
+                                        <Link to={Path.EditProduct} className="btn btn-secondary mx-2 px-4 rounded-pill">Edit</Link>,
+                                        <button className="btn btn-danger rounded-pill px-4" onClick={deleteProductHandler}>Delete</button>
+                                    )}
                                 </div>
                                 <h5 className="box-title mt-5">General information: </h5>
                                 <ul className={styles["list-unstyled"]}>
