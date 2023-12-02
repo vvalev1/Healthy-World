f28b02877185e556e0aaf008dd3b09f68a48520f
Valentin Valev <valio.94.v@gmail.com>
2023-12-02 20:57:38 +0200

    get rid of console. log

--- client/src/utils/pathToUrl.js
+++ client/src/utils/pathToUrl.js
@@ -1,6 +1,5 @@
 export const pathToUrl = (path, params) => {
     const url = Object.keys(params).reduce((result, param) => {
-        console.log(params[param])
         return result.replace(`:${param}`, params[param]);
     }, path);
 
