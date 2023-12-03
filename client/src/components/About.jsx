72c7ea63fc725b166777add7bab9b98c460ff6d5
Valentin Valev <valio.94.v@gmail.com>
2023-12-03 17:45:16 +0200

    edit about component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -20,7 +20,6 @@ export default function About() {
                             <p><i className="fa fa-check text-primary me-3"></i>Tempor erat elitr rebum at clita</p>
                             <p><i className="fa fa-check text-primary me-3"></i>Aliqu diam amet diam et eos</p>
                             <p><i className="fa fa-check text-primary me-3"></i>Clita duo justo magna dolore erat amet</p>
-                            <a className="btn btn-primary rounded-pill py-3 px-5 mt-3" href="">Read More</a>
                         </div>
                     </div>
                 </div>
