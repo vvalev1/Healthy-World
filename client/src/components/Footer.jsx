83ee514144d8406ed8b4d8e023b8b3a0e6708fea
Valentin Valev <valio.94.v@gmail.com>
2023-12-03 17:43:01 +0200

    edit footer visualization

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
+import Path from "../paths/paths";
+
 export default function Footer() {
     return(
         <div className="container-fluid bg-dark footer pt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div className="container py-5">
+        <div className="container py-1">
             <div className="row g-5">
-                <div className="col-lg-3 col-md-6">
+                <div className="col-lg-6 col-md-6">
                     <h1 className="fw-bold text-primary mb-4">H<span className="text-secondary">ea</span>lthy W<span className="text-secondary">or</span>ld</h1>
-                    <p>Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita</p>
+                    <p>Healthy products are important for living healthy life. You can find them only in Healthy World, only fresh fruit and vegetables!</p>
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <h4 className="text-light mb-4">Address</h4>
@@ -15,19 +19,10 @@ export default function Footer() {
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <h4 className="text-light mb-4">Quick Links</h4>
-                    <a className="btn btn-link" href="">About Us</a>
-                    <a className="btn btn-link" href="">Contact Us</a>
-                    <a className="btn btn-link" href="">Our Services</a>
-                    <a className="btn btn-link" href="">Terms & Condition</a>
-                    <a className="btn btn-link" href="">Support</a>
-                </div>
-                <div className="col-lg-3 col-md-6">
-                    <h4 className="text-light mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                    <div className="position-relative mx-auto">
-                        <input className="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email"/>
-                        <button type="button" className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
+                    <Link className="btn btn-link" to={Path.Products}>Products</Link>
+                    <Link className="btn btn-link" to={Path.Login}>Login</Link>
+                    <Link className="btn btn-link" to={Path.Register}>Register</Link>
+                    <Link className="btn btn-link" to={Path.About}>About Us</Link>
                 </div>
             </div>
         </div>
@@ -35,7 +30,7 @@ export default function Footer() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                        &copy; <a href="#">Healthy World</a>, All Right Reserved.
                     </div>
                     <div className="col-md-6 text-center text-md-end">
                         <div>Designed By <a href="https://htmlcodex.com">HTML Codex</a></div>
